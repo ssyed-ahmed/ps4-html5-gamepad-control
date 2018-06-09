3399d043d6e9f2cc456bf83aafa7387896777726
unknown <sumair_ahmed4@yahoo.co.in>
2018-06-09 19:07:39 +1000

    Added socket.io support

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
-const server = require('http').createServer(app).listen(3000, () => {
-  console.log('Example app listening on port 3000')
-})
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 // Listen to web GET requests with the index.html file
 app.get('/', (request, response) => {
   response.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+})
+
+http.listen(3000, () => {
+  console.log('listening on port 3000');
 })
