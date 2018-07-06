03b73c4d6771493579b0960b80ec8db1bce79424
unknown <sumair_ahmed4@yahoo.co.in>
2018-07-06 19:51:57 +1000

    Added rf24 library to wirelessly transmit PS4 gamepad data

--- arduino/web_to_serial.ino
+++ arduino/web_to_serial.ino
@@ -1,8 +1,19 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8); // CE, CSN
+
+const byte address[6] = "00001";
 
 String inputString = "";
 boolean stringComplete = false;
 
 void setup() {
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_HIGH);
+  radio.stopListening();
   Serial.begin(9600);
   inputString.reserve(200);
   Serial.println("Hello from arduino");
@@ -11,6 +22,11 @@ void setup() {
 void loop() {
   if (stringComplete) {
     Serial.println(inputString + "\r\n");
+    //Transmit the inputString using the radio
+    const char text[8];
+    inputString.toCharArray(text, 8);
+    radio.write(&text, sizeof(text));
+    delay(500);
     //Clear the string
     inputString = "";
     stringComplete = false;
@@ -27,4 +43,3 @@ void serialEvent() {
     }
   }
 }
-
